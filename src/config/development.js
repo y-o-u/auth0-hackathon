91049817d0843c61c5735b5abc86823ba5b2f610
ryosuke_mori <ryo5652@gmail.com>
2020-08-09 22:03:37 +0900

    delete files

--- src/config/development.js
+++
@@ -1,4 +0,0 @@
-export default {
-  api_endpoint: 'http://127.0.0.1:8080',
-  return_to: 'http://localhost:3001'
-}
--- src/views/NotFound.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div>
-    お探しのページは見つかりませんでした
-  </div>
-</template>
